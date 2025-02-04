3815ca389c1f69d8980fe239825dd87e68a397d0
... <...>
2025-02-04 23:39:23 +0100

    Add Hyprland Cachix

--- hosts/latitude/system/default.nix
+++ hosts/latitude/system/default.nix
@@ -89,5 +89,10 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
 
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+  nix.settings = {
+    experimental-features = ["nix-command" "flakes"];
+
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
 }
