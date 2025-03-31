929524e68ca6086ee32a6a6fe68d6a0511397be4
matyashorvath <99116949+matyashorvath@users.noreply.github.com>
2025-03-31 02:28:52 +0200

    Removed unnecessary functions from script

--- hosts/latitude/home/scripts/switch-wallpaper.sh
+++ hosts/latitude/home/scripts/switch-wallpaper.sh
@@ -1,31 +1,24 @@
 #!/usr/bin/env bash
 
 picturePath="$HOME/Pictures/Wallpapers"
+currentWallpaper=$(hyprctl hyprpaper listactive | sed -ne "1p" | awk '{print $2}' | awk -F"/" '{print $NF}')
+wpArr=()
+currentWpNum=0
 
-getWallpapers() {
-    wpArr=()
-    for (( i=0; ; i++ )); do
-        element=$(ls ${picturePath} | sed -ne "$(( i+1 ))p")
-        if [ "${element}" == "" ]; then
-            break
-        fi
-        wpArr[${i}]=$element
-    done
-}
-
-getCurrentWpNum() {
-    currentWpNum=0
-    currentWallpaper=$(hyprctl hyprpaper listactive | sed -ne "1p" | awk '{print $2}' | awk -F"/" '{print $NF}')
-    for (( i=0; i<${#wpArr[*]}; i++ )); do
-        if [ "${currentWallpaper}" == "${wpArr[${i}]}" ]; then
-            currentWpNum=$i
-            break
-        fi
-    done
-}
-
-getWallpapers
-getCurrentWpNum
+for (( i=0; ; i++ )); do
+    element=$(ls ${picturePath} | sed -ne "$(( i+1 ))p")
+    if [ "${element}" == "" ]; then
+        break
+    fi
+    wpArr[${i}]=$element
+done
 wpArrLen=${#wpArr[*]}
+    
+for (( i=0; i<${#wpArr[*]}; i++ )); do
+    if [ "${currentWallpaper}" == "${wpArr[${i}]}" ]; then
+        currentWpNum=$i
+        break
+    fi
+done
 
 hyprctl hyprpaper reload ,$picturePath/${wpArr[$(( (currentWpNum+1) % wpArrLen ))]}
