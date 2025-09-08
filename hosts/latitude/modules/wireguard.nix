71a0ae7654bf512dc933f5cd4b31d2aaaee8cfae
matyashorvath <99116949+matyashorvath@users.noreply.github.com>
2025-09-08 21:30:54 +0200

    Updated wireguard config

--- hosts/latitude/modules/wireguard.nix
+++ hosts/latitude/modules/wireguard.nix
@@ -18,13 +18,13 @@
           {
             publicKey = builtins.readFile ../../../secrets/wg-keys/rpi.pub;
             allowedIPs = ["10.0.0.0/32"];
-            endpoint = "floppa.dnet.hu:39680";
+            endpoint = "floppa2.dnet.hu:39680";
             persistentKeepalive = 25;
           }
           {
             publicKey = builtins.readFile ../../../secrets/wg-keys/pc.pub;
             allowedIPs = ["10.0.0.2/32"];
-            endpoint = "floppa.dnet.hu:39681";
+            endpoint = "floppa2.dnet.hu:39681";
             persistentKeepalive = 25;
           }
         ];
